8be4fdb2aa62b0aa2c03738b5cbf2eb35fd1b190
jeffery durand <jeff.durand@gmail.com>
2018-08-14 10:53:32 -0400

    easier api include

---
+++ javascript/api_client_async/index.js
@@ -0,0 +1,21 @@
+const ApiClient = require('../src/ApiClient')
+
+class PromiseApiClient extends ApiClient {
+
+  callApi(...args) {
+    const superMethod = super.callApi.bind(this)
+    const cb = args.pop()
+    const promiseFunc = (resolve, reject) => {
+      superMethod(...args, (err, data) => {
+        if(err) {
+          reject(err)
+        }
+        resolve(data)
+      })
+    }
+    return new Promise(promiseFunc)
+  }
+
+}
+
+module.exports = PromiseApiClient
